b15ac6f8590d2eb9e0455c37d0116e4937a084d0
zxy <1468439019@qq.com>
2024-04-21 19:45:02 +0800

    完成练习4.21

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples (price:u32) -> u32{
+    if price<41 {
+       return price*2
+    }
+    price
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
